52880847899563307f3074327efe48583b8b5e1b
jscuster <jscuster@gmail.com>
2013-12-11 22:08:39 -0800

    oops, the file I've been changing all this time was not added.

---
+++ scripts/webtracker.js
@@ -0,0 +1,98 @@
+var WebTracker = WebTracker || {};
+if (window.File && window.FileReader && window.FileList && window.Blob && (window.AudioContext || window.webkitAudioContext)) {
+$(function() {
+'use strict';
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		var context = new window.AudioContext(),
+changed = false,
+song = WebTracker.emptySong(),
+initialized = false,
+filename = "untitled",
+updateCallbacks = [],
+
+showPanel = function(name) {
+$(".mainPanel").hide();
+$("#" + name).show();
+}, //showPanel
+
+showSubpanel = function(name) {
+$(".subpanel").hide();
+$("#" + name).show();
+}, //showPanel
+
+update = function() {
+var l = updateCallbacks.length;
+for (var i = 0; i < l; i++) {
+updateCallbacks[i]();
+} //i
+}; //updatesafter changes are made.
+
+updateCallbacks.push(function() { //file opened
+$("#filesFilename").html("current file: " + filename + " - " + song.title)
+});
+
+updateCallbacks.push(function() {
+document.title = song.title + ": Web Tracker";
+$("#songTitle").prop('value', song.title)
+}); //make sure the title reflects the song title
+
+$(".menu").click(function() {
+showPanel($(this).html().toLowerCase());
+$(".menu").removeAttr("disabled");
+$(this).attr("disabled", "disabled");
+$("#subfirst").click();
+}); //click
+
+$(".submenu").click(function() {
+showSubpanel($(this).html().toLowerCase());
+$(".submenu").removeAttr("disabled");
+$(this).attr("disabled", "disabled");
+}); //click
+$("#first").click();
+
+$("#fileOpen").change(function(e) {
+var f = e.target.files[0]; //only open the first selected file
+					var reader = new FileReader();
+					reader.onload = function (e) {
+						var dv = new DataView(e.target.result);
+						var sng = WebTracker.modLoader(dv, context);
+if (sng) {
+song = sng;
+filename = f.name;
+update();
+initialized = true;
+changed = false;
+} else {
+alert(f.name + " is an invalid amiga module. Please select only amiga modules (*.mod).");
+} //else
+}; //onload
+reader.readAsArrayBuffer(f);
+}); //fileOpen change (in file/open menu
+
+$("#newFile").click(function () {
+var go = true;
+if (changed) {
+go = confirm("This will erace your current song. Do you wish to proceed?");
+} //if changed
+if (go) { //user says OK or the changes were saved.
+changed = false;
+song = WebTracker.emptySong();
+update();
+} //if
+}); //new file creation
+
+$("#songTitle").focusout(function() {
+var t = $(this).prop('value');
+if (t !== song.title) {
+if (confirm("Changing song title to '" + t + "'. Proceed?")) {
+song.title = t;
+update();
+} //if user says yes
+} //if titles don't match
+});
+
+update();
+}); //ready
+} else {
+	alert('Some required features are unavailable in this browser. Please upgradee this browser or use another. We recommend Google Chrome or Mozilla Firefox.');
+} //else
