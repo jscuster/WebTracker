725bc630f61eccc0401751064965a5f8fa4f42a3
jscuster <jscuster@gmail.com>
2014-02-14 20:45:25 -0800

    Updated counter for new file.

--- scripts/count.sh
+++ scripts/count.sh
@@ -1,1 +1,1 @@
-wc -l webtracker.js  wtloader.js  sample.js  sampleManager.js  sampler.js  readAmiga.js writeAmiga.js amigaUtils.js song.js songplayer.js utils.js log.js
+wc -l webtracker.js  wtloader.js  sample.js  sampleManager.js  sampler.js  readAmiga.js writeAmiga.js instrument.js amigaUtils.js song.js songplayer.js utils.js log.js
